d84f6c7105a09281c99858f4569fd9808c5799d1
Major-shaw <atulyasingh2004@gmail.com>
2022-02-17 13:59:01 +0530

    added name to site

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_03_104831) do
+ActiveRecord::Schema.define(version: 2022_02_11_124541) do
 
   create_table "accounts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -27,9 +27,9 @@ ActiveRecord::Schema.define(version: 2022_02_03_104831) do
     t.string "admin_username"
     t.string "admin_password"
     t.string "status", default: "running"
-    t.string "url"
     t.integer "account_id"
     t.string "category"
+    t.string "name"
     t.index ["account_id"], name: "index_sites_on_account_id"
   end
 
